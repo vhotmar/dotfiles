e7366ec68be0fc474fa314aebc3b35b15995a9fc
Vojtech Hotmar <vojtech.hotmar@pm.me>
2025-03-29 15:37:17 +0100

    chore: add home-manager unix configuration

---
+++ nix/homes/x86_64-linux/vhotmar@unix-server/default.nix
@@ -0,0 +1,53 @@
+{ config, ... }:
+
+{
+  vlib = {
+    user = {
+      enable = true;
+      name = config.snowfallorg.user.name;
+    };
+
+    # suites = { common = { enable = true; }; };
+    #
+    # apps = { alacritty = { enable = true; }; };
+    #
+    # cli-apps = {
+    #   emacs = { enable = true; };
+    #   home-manager = { enable = true; };
+    #   lazygit = { enable = true; };
+    #   sesh = { enable = true; };
+    #   nushell = { enable = true; };
+    # };
+    #
+    # tools = {
+    #   powershell = { enable = true; };
+    #   gh = { enable = true; };
+    #   vpn = { enable = true; };
+    #   devenv = { enable = true; };
+    #   cachix = { enable = true; };
+    #   yq = { enable = true; };
+    #   gum = { enable = true; };
+    #   rust-script = { enable = true; };
+    #   entr = { enable = true; };
+    #   dust = { enable = true; };
+    #   procs = { enable = true; };
+    #   jless = { enable = true; };
+    #   oauth2c = { enable = true; };
+    #   azure = { enable = false; };
+    #   helm = { enable = true; };
+    #   pngpaste = { enable = true; };
+    #   golang = { enable = true; };
+    #   bitwarden = { enable = true; };
+    #   kubernetes = { enable = true; };
+    #   python = { enable = true; };
+    #   rqbit = { enable = true; };
+    #   kdoctor = { enable = true; };
+    #   rbenv = { enable = true; };
+    #   ollama = { enable = true; };
+    # };
+  };
+
+  home.sessionPath = [ "$HOME/main/bin" ];
+
+  home.stateVersion = "23.11";
+}
